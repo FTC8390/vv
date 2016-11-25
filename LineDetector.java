cf7d23157a235a76c1637df57560489853694cd0
jaygard0706 <jaygard0706@gmail.com>
2016-11-25 11:54:40 -0500

    Completed Autonomous (Have not tested it though!!!) Also added color detector in ServoTest Added LineDetextor into RobotVV Class

---
+++ LineDetector.java
@@ -0,0 +1,36 @@
+package ftc8390.vv;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.LightSensor;
+
+/**
+ * Created by jmgu3 on 11/25/2016.
+ */
+
+public class LineDetector {
+
+    LightSensor left;
+    LightSensor middle;
+    LightSensor right;
+
+    public void init (HardwareMap hardwareMap)
+    {
+        left = hardwareMap.lightSensor.get("left");
+        middle = hardwareMap.lightSensor.get("middle");
+        right = hardwareMap.lightSensor.get("right");
+        left.enableLed(true);
+        middle.enableLed(true);
+        right.enableLed(true);
+    }
+
+
+    public boolean lineIsFoundInMiddle()
+    {
+        if(middle.getLightDetected() > .5 )
+            return true;
+        else
+            return false;
+    }
+
+
+}
