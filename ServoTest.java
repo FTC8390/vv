d3f74c239c24921e315fe6792a7f349f5d83d5a2
jaygard0706 <jaygard0706@gmail.com>
2016-11-25 10:29:36 -0500

    Added Telemetry Data for ServoTest and also added Loader servo

--- ServoTest.java
+++ ServoTest.java
@@ -26,6 +26,7 @@ public class ServoTest extends OpMode {
         mooMoo.init(hardwareMap);
         leftPusherVar = .5;
         rightPusherVar = .5;
+        loaderPos = .5;
     }
     public ServoTest()
     {
@@ -37,11 +38,11 @@ public class ServoTest extends OpMode {
      {
          if(gamepad1.left_bumper)
          {
-             leftPusherVar -= .01;
+             leftPusherVar -= .001;
          }
          if(gamepad1.left_trigger > .5)
          {
-             leftPusherVar += .01;
+             leftPusherVar += .001;
          }
          if(leftPusherVar < 0)
              leftPusherVar = 0;
@@ -51,11 +52,11 @@ public class ServoTest extends OpMode {
 
          if(gamepad1.right_bumper)
          {
-             rightPusherVar -= .01;
+             rightPusherVar -= .001;
          }
          if(gamepad1.right_trigger > .5)
          {
-             rightPusherVar += .01;
+             rightPusherVar += .001;
          }
          if(rightPusherVar < 0)
              rightPusherVar = 0;
@@ -63,7 +64,24 @@ public class ServoTest extends OpMode {
              rightPusherVar = 1;
          mooMoo.beaconPusher.rightPusher.setPosition(rightPusherVar);
 
+        if(gamepad1.dpad_up)
+        {
+            loaderPos += .001;
+        }
+         if(gamepad1.dpad_down)
+         {
+             loaderPos -= .001;
+         }
 
+         if(loaderPos < 0)
+             loaderPos = 0;
+         if(loaderPos > 1)
+             loaderPos = 1;
+         mooMoo.loader.servo.setPosition(loaderPos);
+
+         telemetry.addData("Right Beacon Pusher Position: " + mooMoo.beaconPusher.rightPusher.getPosition() ,  " Right Trigger is Up, Bumper is Down ");
+         telemetry.addData("Left Beacon Pusher Position: " + mooMoo.beaconPusher.leftPusher.getPosition() ,  " Left Trigger is Up, Bumper is Down ");
+         telemetry.addData("Loader Position: " + mooMoo.loader.servo.getPosition() ,  " Dpad_Up is Up, Dpad_Down is Down ");
      }
 
 }
