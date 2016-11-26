bd26b8b1d13036d4cdc4c2a7b393ac71f8c6e52b
ftc8390 <ftc8390@gmail.com>
2016-11-26 06:58:02 -0500

    Changed name of loader servo

--- Loader.java
+++ Loader.java
@@ -7,15 +7,17 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Created by jmgu3 on 11/7/2016.
  */
 public class Loader {
-   public Servo servo;
+    public Servo servo;
 
     public void init(HardwareMap hardwareMap) {
-        servo = hardwareMap.servo.get("servo");
+        servo = hardwareMap.servo.get("loader");
     }
-    public void raise(){
+
+    public void raise() {
         servo.setPosition(.25);
     }
-    public void lower(){
+
+    public void lower() {
         servo.setPosition(.75);
     }
 }
